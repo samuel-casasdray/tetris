6e2698a7efdde1c126a8e5a3304a6adcecc95136
Luca <coduri.vitor@gmail.com>
2024-06-08 02:12:40 +0200

    feature: boardToScreen

--- tetris_client/src/main.rs
+++ tetris_client/src/main.rs
@@ -1,13 +1,14 @@
 use bevy::prelude::*;
 use bevy::sprite::{MaterialMesh2dBundle, Mesh2dHandle};
 
-const BOARD_WIDTH:usize = 10;
-const BOARD_HEIGHT:usize = 20;
-static BLOCK_SIZE:f32 = 50.;
+const BOARD_WIDTH: usize = 10;
+const BOARD_HEIGHT: usize = 20;
+static BLOCK_SIZE: f32 = 50.;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, (setup_game,renderBoard, renderBlocks).chain())
+        .add_systems(Startup, (setup_game, renderBoard, renderBlocks).chain())
         // .add_systems(Update, renderBlocks)
         .run()
 }
@@ -17,17 +18,19 @@ fn main() {
 struct MyColor(String);
 
 #[derive(Component)]
-struct Board{
+struct Board {
     blocks: Vec<Option<MyColor>>,
 }
 
 #[derive(Component)]
-struct Shape {
+struct Shape {}
 
-}
-
-fn setup_game(mut commands: Commands){
-    commands.spawn(Camera2dBundle::default());
+fn setup_game(mut commands: Commands) {
+    let cam_position = boardToScreen(BOARD_WIDTH as f32, BOARD_HEIGHT as f32, None);
+    commands.spawn(Camera2dBundle{
+        transform: Transform::from_xyz(cam_position.0 / 2., cam_position.1 / 2., 0.0),
+        ..default()
+    });
 
     let mut blocks = vec![None; BOARD_WIDTH * BOARD_HEIGHT];
 
@@ -48,26 +51,35 @@ fn renderBoard(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-){
+) {
     let wall_width = 5.;
-    let horizontal_bar = Mesh2dHandle(meshes.add(Rectangle::new(BLOCK_SIZE * BOARD_WIDTH as f32 + BLOCK_SIZE, wall_width)));
-    let vertical_bar = Mesh2dHandle(meshes.add(Rectangle::new(wall_width, BLOCK_SIZE * BOARD_HEIGHT as f32 + BLOCK_SIZE)));
+
+    let horizontal_bar_width = BLOCK_SIZE * BOARD_WIDTH as f32;
+    let vertical_bar_height = BLOCK_SIZE * BOARD_HEIGHT as f32;
+
+    let horizontal_bar = Mesh2dHandle(meshes.add(Rectangle::new(horizontal_bar_width + BLOCK_SIZE, wall_width)));
+    let vertical_bar = Mesh2dHandle(meshes.add(Rectangle::new(wall_width, vertical_bar_height + BLOCK_SIZE)));
 
     let color = Color::hex("FFFFFF").unwrap();
 
+    let top_position = boardToScreen(0., BOARD_HEIGHT as f32, Some(Offset(horizontal_bar_width / 2., BLOCK_SIZE / 2.)));
+    let bottom_position = boardToScreen(0., 0., Some(Offset(horizontal_bar_width / 2., BLOCK_SIZE / -2.)));
+    let left_position = boardToScreen(0., 0., Some(Offset(BLOCK_SIZE / -2., vertical_bar_height / 2.)));
+    let right_position = boardToScreen(BOARD_WIDTH as f32, 0., Some(Offset(BLOCK_SIZE / 2., vertical_bar_height / 2.)));
+
     let shapes = [
-        (horizontal_bar.clone(), 0., BLOCK_SIZE * BOARD_HEIGHT as f32 / 2. + BLOCK_SIZE / 2.),
-        (horizontal_bar, 0., -BLOCK_SIZE * BOARD_HEIGHT as f32 / 2. - BLOCK_SIZE / 2.),
-        (vertical_bar.clone(), -BLOCK_SIZE * BOARD_WIDTH as f32 / 2. - BLOCK_SIZE / 2., 0.),
-        (vertical_bar, BLOCK_SIZE * BOARD_WIDTH as f32 / 2. + BLOCK_SIZE / 2., 0.),
+        (horizontal_bar.clone(), top_position.0, top_position.1),
+        (horizontal_bar, bottom_position.0, bottom_position.1),
+        (vertical_bar.clone(), left_position.0, left_position.1),
+        (vertical_bar, right_position.0, right_position.1),
     ];
 
-    for (shape, x,y) in shapes {
-        commands.spawn(MaterialMesh2dBundle{
+    for (shape, x, y) in shapes {
+        commands.spawn(MaterialMesh2dBundle {
             mesh: shape,
             material: materials.add(color),
             transform: Transform {
-                translation: Vec3::new(x,y,0.),
+                translation: Vec3::new(x, y, 0.),
                 scale: Vec3::splat(1.),
                 ..default()
             },
@@ -76,13 +88,24 @@ fn renderBoard(
     }
 }
 
+
+struct Offset(f32, f32);
+
+// Ne marche pas encore comme je le souhaite
+fn boardToScreen(x: f32, y: f32, offset: Option<Offset>) -> (f32, f32) {
+    match offset {
+        None => (x * BLOCK_SIZE, y * BLOCK_SIZE),
+        Some(Offset(x_offset, y_offset)) => (x * BLOCK_SIZE + x_offset, y * BLOCK_SIZE + y_offset),
+    }
+}
+
 fn renderBlocks(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     board: Query<&Board>,
-){
-    for (index, color) in board.single().blocks.iter().enumerate(){
+) {
+    for (index, color) in board.single().blocks.iter().enumerate() {
         if let Some(MyColor(color)) = color {
             let x = (index % BOARD_WIDTH) as f32;
             let y = (index / BOARD_WIDTH) as f32;
@@ -90,11 +113,11 @@ fn renderBlocks(
             let shape = Mesh2dHandle(meshes.add(Rectangle::new(BLOCK_SIZE, BLOCK_SIZE)));
             let color = Color::hex(color);
 
-            commands.spawn(MaterialMesh2dBundle{
+            commands.spawn(MaterialMesh2dBundle {
                 mesh: shape,
                 material: materials.add(color.unwrap()),
                 transform: Transform {
-                    translation: Vec3::new(x*BLOCK_SIZE, -y * BLOCK_SIZE, 0.),
+                    translation: Vec3::new(x * BLOCK_SIZE, -y * BLOCK_SIZE, 0.),
                     scale: Vec3::splat(1.),
                     ..default()
                 },
