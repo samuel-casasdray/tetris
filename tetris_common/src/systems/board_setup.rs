2c39e5664e7811b48dff12a2e7e389fe3c0e431b
Samuel CASAS DRAY <samuelcasasdray@gmail.com>
2024-06-10 18:03:18 +0200

    fill board

--- tetris_common/src/systems/board_setup.rs
+++ tetris_common/src/systems/board_setup.rs
@@ -8,7 +8,7 @@ pub fn setup_board(mut commands: Commands) {
         .id();
 
     let block_entities: Vec<Entity> = (0usize..10usize)
-        .zip(0usize..20usize)
+        .flat_map(|x| (0usize..20usize).map(move |y| (x, y)))
         .map(|(x, y)| {
             commands
                 .spawn((
